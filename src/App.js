38fa211c3fe36e497bff2b47296c61cf6bd37c9e
Mark-Malak <markmalak71@gmail.com>
2021-04-25 22:04:15 +0200

    essential files

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import './App.css';
+
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+
+import { fetchData } from './api';
+
+class App extends React.Component{
+  state = {
+    data : {} ,
+
+
+
+  }
+
+
+
+  async componentDidMount (){
+    const fetchedData = await fetchData();
+    
+    this.setState( {data : fetchedData}   );
+  }
+
+  render(){
+    const {data} = this.state ; 
+
+
+
+    return (
+      <div className="container">
+        <Cards  data = {data}/>
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
+
+ 
+
+
+
+}
+
+export default App;
